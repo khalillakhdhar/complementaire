19c3a3b9b7f3d0630b030e95119b9a241cbb08a6
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2025-02-23 20:29:22 +0100

    updated

--- src/app/admin/tasks/tasks.component.ts
+++ src/app/admin/tasks/tasks.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../../shared/services/task.service';
+import { Task } from '../../shared/models/task';
+import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-tasks',
@@ -7,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
+  task={} as Task;
+  tasks:Task[]=[];
+  grade=""
+  constructor(private taskService:TaskService,private userService:UserService)
+  {
+    this.tasks=this.taskService.getTasks();
+    let current=this.userService.getOneUser(Number(localStorage.getItem('currentUser')));
+    this.grade=current.role;
+
+  }
+  addTask()
+  {
+    this.taskService.addTask(this.task);
+    this.tasks=this.taskService.getTasks();
+    this.task={} as Task;
+  }
+  deleteTask(index:number)
+  {
+    if(confirm("vous voulez supprimer cette tache? "))
+    this.taskService.deleteTask(index);
+    this.tasks=this.taskService.getTasks();
+  }
+
 
 }
